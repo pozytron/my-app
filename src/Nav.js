9b0339c68d8b61ec40030a8c2425a00e17a6415e
tymulamichal <michal.tymula@amsterdamstandard.com>
2022-08-10 18:21:15 +0200

    cleanup

--- src/Nav.js
+++ src/Nav.js
@@ -1,40 +1,40 @@
-import React,{Component} from "react";
+import React, { Component } from "react";
 import './index.css'
 
 class Navigation extends Component {
     render() {
-        const arr = ['click me','or me','or me!']
+        const arr = ['click me', 'or me', 'or me!']
         return (
             <nav>
-                <NavItem value={arr[0]}/>
-                <NavItem value={arr[1]}/>
-                <NavItem value={arr[2]}/>
+                <NavItem value={arr[0]} />
+                <NavItem value={arr[1]} />
+                <NavItem value={arr[2]} />
             </nav>
         )
     }
 }
 
 class NavItem extends Component {
-        state = {
-            value: this.props.value,
-            flag: true
-        }
+    state = {
+        value: this.props.value,
+        flag: true
+    }
 
-    handleClick = ()=>{
-        this.setState((prev)=>{
+    handleClick = () => {
+        this.setState((prev) => {
             const newValue = prev.flag ? 'changed' : this.props.value
             return {
-                value:newValue,
-                flag:!prev.flag
+                value: newValue,
+                flag: !prev.flag
             }
         })
     }
 
     render() {
         return (
-            <ClickableThing 
-            onClick={this.handleClick}
-            value= {this.state.value}
+            <ClickableThing
+                onClick={this.handleClick}
+                value={this.state.value}
             />
         )
     }
